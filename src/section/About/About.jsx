2c88a7dd073b542ad7ef74c79b406ff444e3c883
Guillaume Debas <guillaumedebas@hotmail.fr>
2023-09-12 00:20:01 +0200

    modification of the about section

--- src/section/About/About.jsx
+++ src/section/About/About.jsx
@@ -1,5 +1,7 @@
 import "./About.scss"
 import Social from "../../components/Social/Social"
+
+import "../../assets/guillaume_debas.pdf"
 /**
  * About component.
  * Displays the about section of the website.
@@ -12,12 +14,18 @@ function About() {
         À propos
       </h2>
      <p className="about__content">
-     Issu d'une reconversion réussie, j’ai acquis de solide connaissance dans la conception et le développement d'applications Web modernes.
-     </p>
+     Issu d'une reconversion réussie, j’ai acquis de solide connaissance dans la conception et le développement d'applications Web modernes. </p>
      <p className="about__content">
-     Passionné et créatif, je suis animé par la volonté de contribuer activement à la création du monde numérique de demain.
+J'accorde une grande importance à bien comprendre les besoins spécifiques de chaque projet.
+La collaboration au sein d'une équipe est une facette que j'apprécie particulièrement.
+Je veille à mettre en place des solutions accessibles et maintenables, tout en préservant la performance des projets.</p>
+<p className="about__content">
+Passionné et créatif, je suis animé par la volonté de contribuer activement à la création du monde numérique de demain.
+Si vous recherchez un développeur Web avec ces valeurs, contactez-moi.
      </p>
-     <Social />
+     
+
+     <a href="src/assets/guillaume_debas_cv.pdf" target="_blank" className="about__cv">Télécharger mon CV</a>
     </section>
   )
 }
