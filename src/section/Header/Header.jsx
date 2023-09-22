fe5367c337c5a99701dd861c28ccfe07df025cd3
Guillaume Debas <guillaumedebas@hotmail.fr>
2023-09-22 15:18:25 +0200

    ios css fix

--- src/section/Header/Header.jsx
+++ src/section/Header/Header.jsx
@@ -21,13 +21,13 @@ function Header() {
     setTimeout(() => {
       setIsMenuOpenIcon(!isMenuOpenIcon)
       setIsActive(true)
-    }, 201)
+    }, 101)
 
     if (!isMenuOpen) {
       setIsMenuOpen(!isMenuOpen)
       setTimeout(() => {
         setIsActiveMenu(true)
-      }, 21)
+      }, 101)
     } else {
       setTimeout(() => {
         setIsMenuOpen(!isMenuOpen)
@@ -46,25 +46,27 @@ function Header() {
           <Photo />
         </a>
         <ul className="menu--desktop">
-            <li key="link1"><a href="#about">À propos<span className="underline-effect"></span></a></li>
-            <li key="link2"><a href="#technology">Technologies<span className="underline-effect"></span></a></li>
-            <li key="link3"><a href="#project">Projets<span className="underline-effect"></span></a></li>
-            <li key="link4"><a href="#contact">Contact<span className="underline-effect"></span></a></li>
-          </ul>
-        <button className={`${isMenuOpenIcon ? "cross" : "hamburger"} ${isActive ? "active" : ""}`} onClick={toggleMenu}>X</button>
+          <li key="link1"><a href="#about">À propos<span className="underline-effect"></span></a></li>
+          <li key="link2"><a href="#technology">Technologies<span className="underline-effect"></span></a></li>
+          <li key="link3"><a href="#project">Projets<span className="underline-effect"></span></a></li>
+          <li key="link4"><a href="#contact">Contact<span className="underline-effect"></span></a></li>
+        </ul>
+        <button className={`${isMenuOpenIcon ? "cross" : "hamburger"} ${isActive ? "active" : ""}`} onClick={toggleMenu}>.</button>
 
 
       </nav>
 
       {isMenuOpen && (
-        <div className="menu">
-          <ul className={`${isActiveMenu ? "active" : ""}`}>
-            <li key="link1"><a href="#about">À propos</a></li>
-            <li key="link2"><a href="#technology">Technologies</a></li>
-            <li key="link3"><a href="#project">Projets</a></li>
-            <li key="link4"><a href="#contact">Contact</a></li>
-          </ul>
-        </div>
+    
+          <div className="menu">
+            <ul className={`${isActiveMenu ? "active" : ""}`}>
+              <li key="link1"><a href="#about">À propos</a></li>
+              <li key="link2"><a href="#technology">Technologies</a></li>
+              <li key="link3"><a href="#project">Projets</a></li>
+              <li key="link4"><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+
       )}
     </header>
   )
