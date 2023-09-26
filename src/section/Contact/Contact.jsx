f8d44e83f7a25cdfb3d26b112af7c975891ec950
Guillaume Debas <guillaumedebas@hotmail.fr>
2023-09-26 16:42:20 +0200

    Nav menu to component

--- src/section/Contact/Contact.jsx
+++ src/section/Contact/Contact.jsx
@@ -37,7 +37,7 @@ export default function Contact() {
           setNotificationMessage('');
           setNotificationType('');
         }, 500);
-      }, 1003500);
+      }, 3500);
 
       return () => clearTimeout(timer); // Clear the timer if the component unmounts
     }
--- src/section/Header/Header.jsx
+++ src/section/Header/Header.jsx
@@ -1,90 +1,20 @@
-import "./Header.scss"
-import Photo from "../../components/Photo/Photo"
-import React, { useState } from 'react';
-
-
-
-/**
- * Hearder component.
- * Displays the header section of the website.
- */
+// Header.js
+import "./Header.scss";
+import Photo from "../../components/Photo/Photo";
+import React from 'react';
+import NavigationMenu from "../../components/NavigationMenu/NavigationMenu";
 
 function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isMenuOpenIcon, setIsMenuOpenIcon] = useState(false);
-  const [isActive, setIsActive] = useState(true);
-  const [isActiveMenu, setIsActiveMenu] = useState(true);
-
-  const toggleMenu = () => {
-    setIsActiveMenu(false)
-    setIsActive(false)
-    setTimeout(() => {
-      setIsMenuOpenIcon(!isMenuOpenIcon)
-      setIsActive(true)
-    }, 101)
-
-    if (!isMenuOpen) {
-      setIsMenuOpen(!isMenuOpen)
-      setTimeout(() => {
-        setIsActiveMenu(true)
-      }, 101)
-    } else {
-      setTimeout(() => {
-        setIsMenuOpen(!isMenuOpen)
-        setTimeout(() => {
-          setIsActiveMenu(true)
-        }, 21)
-      }, 1001)
-    }
-  }
-
-  const hideMenu = () => {
-    const menuElement = document.querySelector('.menu');
-    if (menuElement) {
-      menuElement.classList.add('no-display');
-    }
-   
-    setTimeout(() => {
-      toggleMenu()
-      setTimeout(() => {
-        menuElement.classList.remove('no-display')
-      }, 1000)
-
-    }, 1000)
-  };
-
-
   return (
     <header>
       <nav className="navbar">
         <a href="#start" className="navbar__avatar">
           <Photo />
         </a>
-        <ul className="menu--desktop">
-          <li key="link1" ><a href="#about">À propos<span className="underline-effect"></span></a></li>
-          <li key="link2"><a href="#technology">Technologies<span className="underline-effect"></span></a></li>
-          <li key="link3"><a href="#project">Projets<span className="underline-effect"></span></a></li>
-          <li key="link4"><a href="#contact">Contact<span className="underline-effect"></span></a></li>
-        </ul>
-        <button className={`${isMenuOpenIcon ? "cross" : "hamburger"} ${isActive ? "active" : ""}`} onClick={toggleMenu}>.</button>
-
-
+        <NavigationMenu />
       </nav>
-
-      {isMenuOpen && (
-
-        <div className="menu">
-          <ul className={`${isActiveMenu ? "active" : ""}`}>
-            <li key="link1" ><a href="#about" onClick={hideMenu}>À propos</a></li>
-            <li key="link2" ><a href="#technology" onClick={hideMenu}>Technologies</a></li>
-            <li key="link3" ><a href="#project" onClick={hideMenu}>Projets</a></li>
-            <li key="link4" ><a href="#contact" onClick={hideMenu}>Contact</a></li>
-          </ul>
-        </div>
-
-      )}
     </header>
   )
 }
 
-export default Header
+export default Header;
